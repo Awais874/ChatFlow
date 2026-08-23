bc8b5206dfcedb111f066bb4e871e26e7421ef37
Awais874 <Awais.abdullah99@gmail.com>
2026-08-23 19:50:12 +0200

    fix: remove debug hack, typing indicator rendering confirmed

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -331,8 +331,8 @@ function Chat() {
 
 
 
-                {(typingUsers.length > 0 || true) && (
-                // {typingUsers.length > 0 && (
+                
+                 {typingUsers.length > 0 && (
                   <div style={s.typingIndicator}>
                     <div style={s.typingDots}>
                       <span style={{ ...s.dot, animationDelay: '0ms' }} />
